2a0212d7b283856ed48cc39cbdc5f7d98193eb0e
David Byun <byundavid@naver.com>
2023-08-17 07:22:36 +0900

    flutter widget

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//https://dribbble.com/shots/19858341-Finnancial-Mobile-IOS-App
+
 void main() {
   runApp(App());
 }
@@ -11,7 +13,38 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text('Hey, Selena',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 38,
+                            fontWeight: FontWeight.w600,
+                          )),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(0.8),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          )),
+    );
   }
 }
